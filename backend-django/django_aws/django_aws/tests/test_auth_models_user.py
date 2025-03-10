518f458a6e529755873d2ebe18927fd9b42cb654
Ken Salanio <kssalanio@gmail.com>
2025-03-10 15:05:11 +0800

    added more basic tests

--- backend-django/django_aws/django_aws/tests/test_auth_models_user.py
+++ backend-django/django_aws/django_aws/tests/test_auth_models_user.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 
 @pytest.mark.django_db
-def test_user_create():
-    User.objects.create_user(username="john", password="pass1234")
+def test_user_create(django_user_model):
+    django_user_model.objects.create_user(username="john", password="pass1234")
 
-    assert User.objects.count() == 1
-    user = User.objects.first()
+    assert django_user_model.objects.count() == 1
+    user = django_user_model.objects.first()
     assert user.username == "john"
     assert user.is_active is True
     assert user.is_staff is False
